47dec0e4781818cc4c98c94fb4e15d3f2cdb7e3e
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-10-21 10:40:26 -0500

    init commit

---
+++ label-mobile-app/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.label}>Camera API</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontSize: 20,
+  }
+});
